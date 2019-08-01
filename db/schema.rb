b31163a68bab4ea66752da1d3f866dd8168b2eae
daikinagashima <naga_self@yahoo.co.jp>
2019-08-01 17:35:28 +0000

    CRUD messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_01_052704) do
+ActiveRecord::Schema.define(version: 2019_08_01_054513) do
 
-  create_table "messages", force: :cascade do |t|
+  create_table "tasks", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
